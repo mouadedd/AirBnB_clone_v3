83dedaba3539b12b8491a951ffed2abf1a764834
mouadedd <mdx.edd@gmail.com>
2024-02-27 05:11:42 -0800

    add comment

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,8 @@
 """Import blueprint from flask """
 from flask import Blueprint
 
-app_views = Blueprint('base', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+"""blueprint for the Airbnb api """
 
 from api.v1.views.index import *
 from api.v1.views.states import *
