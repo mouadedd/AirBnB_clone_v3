0c8939d02cf88e10da35b1f6219cdf3d2d0b08dd
mouadedd <mdx.edd@gmail.com>
2024-02-27 05:48:58 -0800

    to test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-getting start with the api
-"""
-from os import getenv
+'''Flask web application API.'''
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app_host = getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(getenv('HBNB_API_PORT', '5000'))
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
@@ -19,28 +17,28 @@ CORS(app, resources={'/*': {'origins': app_host}})
 
 @app.teardown_appcontext
 def teardown_flask(exception):
-    '''flask leastner to handle exception'''
+    '''The Flask print exception'''
     storage.close()
 
 
 @app.errorhandler(404)
 def error_404(error):
-    '''Handle not founded pages'''
+    '''Handles the 404 error'''
     return jsonify(error='Not found'), 404
 
 
 @app.errorhandler(400)
 def error_400(error):
-    '''Handle error handler'''
-    output = 'Bad request'
+    '''Handles the 400 HTTP error code.'''
+    ouput = 'Bad request'
     if isinstance(error, Exception) and hasattr(error, 'description'):
         output = error.description
     return jsonify(error=output), 400
 
 
 if __name__ == '__main__':
-    app_host = getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(getenv('HBNB_API_PORT', '5000'))
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(
         host=app_host,
         port=app_port,
